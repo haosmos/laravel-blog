1776304aeab5e7eb181262d229b79ca951fcdfff
chaosmos <vitovich1987@gmail.com>
2023-05-07 23:02:04 +0300

    linked posts and categories

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,35 @@
 <?php
 
-    use App\Models\Post;
-    use Illuminate\Support\Facades\Route;
+  use App\Models\Category;
+  use App\Models\Post;
+  use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+  /*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider and all of them will
+  | be assigned to the "web" middleware group. Make something great!
+  |
+  */
 
   Route::get('/', function () {
+
     return view('posts', [
       'posts' => Post::all()
     ]);
   });
 
-  Route::get('posts/{post}', function ($slug) {
-    return view('post', [
-      'post' => Post::find($slug)
-    ]);
-  })->where('post', '[A-z_\-]+');
+  Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post', compact('post'));
+  });
+
+  Route::get(
+    'categories/{category:slug}',
+    function (Category $category) {
+      return view('posts', [
+        'posts' => $category->posts
+      ]);
+    });
