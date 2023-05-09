a52d91f1af1cb7f88bc9489d3f97f9bd199efd99
chaosmos <vitovich1987@gmail.com>
2023-05-09 16:56:41 +0300

    divided the code into components: post card, main card, header, dropdown menu

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,11 @@
 
   Route::get('/', function () {
     return view('posts', [
-      'posts' => Post::latest()->get()
+      'posts' => Post::latest()->get(),
+      'categories' => Category::all()
 //      'posts' => Post::with('category')->get()
     ]);
-  });
+  })->name('home');
 
   Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', compact('post'));
@@ -29,12 +30,16 @@
 
   Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-      'posts' => $category->posts
+      'posts' => $category->posts,
+      'currentCategory' => $category,
+      'categories' => Category::all()
+
     ]);
-  });
+  })->name('category');
 
   Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-      'posts' => $author->posts
+      'posts' => $author->posts,
+      'categories' => Category::all()
     ]);
   });
