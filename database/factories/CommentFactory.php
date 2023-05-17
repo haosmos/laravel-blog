e4e2c3673c436f766c7d53d1c7ffe0f2cdce175c
chaosmos <vitovich1987@gmail.com>
2023-05-17 23:24:10 +0300

    refactored comments functionality

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,27 +1,33 @@
 <?php
 
-namespace Database\Factories;
+  namespace Database\Factories;
 
-use App\Models\Post;
-use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
+  use App\Models\Comment;
+  use App\Models\Post;
+  use App\Models\User;
+  use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
- */
-class CommentFactory extends Factory
-{
+  /**
+   * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+   */
+  class CommentFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Comment::class;
+
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
-    {
-        return [
-          'post_id' => Post::factory(),
-          'user_id' => User::factory(),
-          'body' => $this->faker->paragraph()
-        ];
+    public function definition(): array {
+      return [
+        'post_id' => Post::factory(),
+        'user_id' => User::factory(),
+        'body' => $this->faker->paragraph()
+      ];
     }
-}
+  }
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -9,19 +9,20 @@
       <x-category-dropdown />
     </div>
 
+    <!-- Other Filters -->
+
     <!-- Search -->
     <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
       <form method="GET" action="/">
-        @if(request('category'))
+        @if (request('category'))
           <input type="hidden" name="category" value="{{ request('category') }}">
         @endif
 
-        <input
-          type="text"
-          name="search"
-          placeholder="Find something"
-          class="bg-transparent placeholder-black font-semibold text-sm"
-          value="{{ request('search') }}"
+        <input type="text"
+               name="search"
+               placeholder="Find something"
+               class="bg-transparent placeholder-black font-semibold text-sm"
+               value="{{ request('search') }}"
         >
       </form>
     </div>
