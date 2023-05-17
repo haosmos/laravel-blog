cbc83765311a7ad2a1cdf38231e96ca6d94172e3
chaosmos <vitovich1987@gmail.com>
2023-05-18 00:09:09 +0300

    started implementing mail subscription functionality

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@
   use App\Http\Controllers\SessionsController;
   use Illuminate\Support\Facades\Route;
 
+  Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+      'apiKey' => config('services.mailchimp.key'),
+      'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('b447ebe516', [
+      'email_address' => 'vitovich1987@gmail.com',
+      'status' => 'subscribed'
+    ]);
+    ddd($response);
+  });
+
+  Route::post('newsletter', NewsletterController::class);
+
   Route::get('/', [PostController::class, 'index'])->name('home');
 
   Route::get('posts/{post:slug}', [PostController::class, 'show']);
