67178fe5b9751806e6f15ef069012f423c6a3ceb
chaosmos <vitovich1987@gmail.com>
2023-05-18 20:07:28 +0300

    implemented mail subscription functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,25 @@
 
   namespace App\Providers;
 
+  use App\Services\MailchimpNewsletter;
+  use App\Services\Newsletter;
   use Illuminate\Database\Eloquent\Model;
   use Illuminate\Support\ServiceProvider;
+  use MailchimpMarketing\ApiClient;
 
   class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
     public function register(): void {
-      //
+      app()->bind(Newsletter::class, function () {
+        $client = (new ApiClient)->setConfig([
+          'apiKey' => config('services.mailchimp.key'),
+          'server' => 'us6'
+        ]);
+
+        return new MailchimpNewsletter($client);
+      });
     }
 
     /**
