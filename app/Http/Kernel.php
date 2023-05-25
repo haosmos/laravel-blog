2ec839253cdb9c961300e4e985148fd662202264
chaosmos <vitovich1987@gmail.com>
2023-05-26 01:03:24 +0300

    refactor: remade authorization

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,6 @@
      */
     protected $middlewareAliases = [
       'auth' => \App\Http\Middleware\Authenticate::class,
-      'admin' => MustBeAdministrator::class,
       'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
       'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
       'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
